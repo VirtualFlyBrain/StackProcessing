8ba495750267d4f77d781fcbd2d59e6d8b11b027
Robbie1977 <robbie.court@gmail.com>
2014-03-23 22:01:19 +0000

    Update loopThroughFiles.sh

--- scripts/loopThroughFiles.sh
+++ scripts/loopThroughFiles.sh
@@ -5,7 +5,7 @@
 # scripts are in the folder one level above)
 set -x
 #Bear in mind the ps itself creates process count = 1, so increasse this by 1
-numProcesses=13
+numProcesses=5
 for file in $1/*nrrd
 do
 	if [[ -f $file ]]; then	
